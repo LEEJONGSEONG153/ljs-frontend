afa7dc4c6793da671a1cdfd208eb9c9a051f4424
jslee5 <js.lee@brycenkorea.co.kr>
2024-07-19 16:11:22 +0900

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# ljs_front
+# ljs-frontend
 
 ## Project setup
 ```
--- src/router/router.js
+++ src/router/router.js
@@ -3,7 +3,9 @@ import {createRouter, createWebHistory } from "vue-router";
 // 로그인 체크
 const checkPermission = (to, from, next) => {
 
-  next('login');
+  //next('login');
+  next();
+  
 
 }
 
@@ -11,18 +13,13 @@ const checkPermission = (to, from, next) => {
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: "/", name:"defaultLayout", redirect: "/main", component: () => import("@/views/DefaultLayout.vue"),
+    { path: "/", name:"defaultLayout", redirect:"/main", component: () => import("@/views/DefaultLayout.vue"),
       children: [
         { path: "main", name:"main", component: () => import("@/views/DefaultLayout.vue"), beforeEnter: checkPermission }
 
-
-
       ]
     },
-    { path: "/login", name:"login", component: () => import("@/views/login.vue")}
-
-
-
+    { path: "/login", name:"login", component: () => import("@/views/login/login.vue")}
   ]
 });
 
