bea654094482a5762ecf66eb10fe196a5b4aed9c
js.lee <js.lee@brycenkorea.co.kr>
2024-07-22 21:51:54 +0900

    동영상 간격주기

--- src/views/temp/Temp1.vue
+++ src/views/temp/Temp1.vue
@@ -1,9 +1,15 @@
 <template>
-    <video v-for="(item, i) in files" :key="i" height="350" width="350" controls>
-        <!-- <source src="../../../public/assets/movie/준수1.mp4" type="video/mp4"> -->
-        <source :src="'http://58.148.100.28/'+item.filePath+'/'+item.fileNm" type="video/mp4">
-        이 문장은 여러분의 브라우저가 video 태그를 지원하지 않을 때 화면에 표시됩니다!
-    </video>
+    <template v-for="(item, i) in files" :key="i" >
+        <div>
+            <video height="350" width="350" controls>
+                <!-- <source src="../../../public/assets/movie/준수1.mp4" type="video/mp4"> -->
+                <source :src="'http://58.148.100.28/'+item.filePath+'/'+item.fileNm" type="video/mp4">
+                이 문장은 여러분의 브라우저가 video 태그를 지원하지 않을 때 화면에 표시됩니다!
+            </video>
+        </div>
+        <br>
+        <br>
+    </template>
 </template>
 
 <script setup>
