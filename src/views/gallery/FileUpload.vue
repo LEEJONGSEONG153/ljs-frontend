55696c67eabe0a257ee157edfaaaca033e5054e2
jslee5 <js.lee@brycenkorea.co.kr>
2024-07-31 11:04:47 +0900

    파일업로드 수정

--- src/views/gallery/FileUpload.vue
+++ src/views/gallery/FileUpload.vue
@@ -1,22 +1,28 @@
 <template>
-    <el-upload
-    class="upload-demo"
-    multiple
-    drag
-    action="#"
-    :before-upload="beforeAvatarUpload"
-    
-    >
-        <el-icon class="el-icon--upload"><upload-filled /></el-icon>
-        <div class="el-upload__text">
-        파일을 끌어 놓으세요
-        <!-- Drop file here or <em>click to upload</em> -->
+    <div :class="{'background' : isSpinner }">
+        <el-upload
+        class="upload-demo"
+        multiple
+        drag
+        action="#"
+        :before-upload="beforeAvatarUpload"
+        
+        >
+            <el-icon class="el-icon--upload"><upload-filled /></el-icon>
+            <div class="el-upload__text">
+            파일을 끌어 놓으세요
+            <!-- Drop file here or <em>click to upload</em> -->
+            </div>
+        </el-upload>
+        <div v-for="(item,index) in uploadFiles" :key="index">
+            <p>{{item.name}}</p>
         </div>
-    </el-upload>
-    <div v-for="(item,index) in uploadFiles" :key="index">
-        <p>{{item.name}}</p>
+        <button type="button" class="btn medium filled" v-if="saveBtnFlag" @click="saveFile">업로드</button>
     </div>
-    <button type="button" class="btn medium filled" v-if="saveBtnFlag" @click="saveFile">업로드</button>
+    <div>
+        <div :class="{'spinner' : isSpinner }"></div>
+        <!-- <easy-spinner :type="'circular'" :size="100" :class="spinner"/> -->
+    </div>
 </template>
 
 <script setup>
@@ -32,13 +38,15 @@ const store = useStore();
 const uploadFiles = ref([]);
 const createDates = ref([]);
 const saveBtnFlag = ref(false);
-
+const isSpinner = ref(false);
 
 /**
  * 업로드 전
  * action 에 적힌 url 타기 전인데 return false로 rawFile 정보 저장
  */
 const beforeAvatarUpload = async(rawFile) => {
+
+    isSpinner.value = true;
     saveBtnFlag.value = false;
 
     console.log('rawFile', rawFile);
@@ -72,6 +80,7 @@ const beforeAvatarUpload = async(rawFile) => {
     
     
     saveBtnFlag.value = true;
+    isSpinner.value = false;
     
     return false;
 }
@@ -92,10 +101,14 @@ const getHeicToJpeg = async (file)=>{
 
 const saveFile = async () => {
 
+
     store.dispatch("confirm/open", {
         title: "파일 업로드",
         message: "파일을 업로드 하시겠습니까?",
         callback: async () => {
+            await store.dispatch("confirm/close")
+
+            isSpinner.value = true;
 
             let formData = new FormData();
 
@@ -116,11 +129,46 @@ const saveFile = async () => {
 
             uploadFiles.value = [];
             createDates.value = [];
+            saveBtnFlag.value = false;
+            isSpinner.value = false;
         }
     })
 }
 </script>
 
 <style>
+.background {
+    display: none;
+    opacity: 0.1;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0,0,0,0.8);
+    z-index: 1000;
+    justify-content: center;
+    align-items: center;
+    margin: -30px 0 0 -30px;
+}
+.spinner {
+    position: absolute;
+    top: 50%;
+    left: 40%;
+    border: 8px solid #f3f3f3; /* Light grey */
+    border-top: 8px solid #3498db; /* Blue */
+    border-radius: 50%;
+    width: 60px;
+    height: 60px;
+    animation: spinner 2s linear infinite;
+}
+@keyframes spinner {
+    0% {
+        transform: rotate(0deg);
+    }
+    100% {
+        transform: rotate(360deg);
+    }
+}
 
 </style>
